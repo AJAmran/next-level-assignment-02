1a5dc2aeae75ea615da14fdcffc934bb2a39fdd3
AJamran <mdamranhossen77@gmail.com>
2026-05-21 14:33:41 +0600

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { type Application } from "express";
 import { userRoute } from "./routes/auth.routes";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 const app: Application = express();
 app.use(express.json());
 
 app.use("/api/auth", userRoute);
 
+
+
+app.use(globalErrorHandler);
 export default app;
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { authController } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.post("/signup", authController.signUpUser);
 router.post("/login", authController.loginUser);
+//test route for protected route
+
+router.get("/test", authMiddleware("contributor"));
 
 export const userRoute = router;
